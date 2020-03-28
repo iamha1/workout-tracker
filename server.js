adba51bddcd844bff4fd6aa672923cd6d06685eb
Ha Nguyen <nguyenthingocha2@gmail.com>
2020-03-27 23:39:13 -0400

    adding README

--- server.js
+++ server.js
@@ -20,35 +20,35 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://workout-tracker-march2020:password1@ds255308.mlab.com:55308/heroku_gth9scw4", { useNewUrlParser: true });
 
 // Routes here
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
     res.sendFile("index.html");
 });
 
-app.get("/exercise", function(req, res) {
+app.get("/exercise", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/exercise.html"));
-  });
+});
 
-app.get("/stats", function(req, res) {
+app.get("/stats", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/stats.html"));
-  });
+});
 
-app.get("/api/workouts", (req,res) => {
-   // console.log("workouts here");
+app.get("/api/workouts", (req, res) => {
+    // console.log("workouts here");
     db.Workout.find({})
-    .then(dbWorkout => {
-        res.json(dbWorkout);
-    })
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
 });
 
 app.post("/api/workouts", (req, res) => {
-db.Workout.create({}) 
-.then(dbWorkout => {
-    //console.log(dbWorkout);
-    res.json(dbWorkout);
-})
-.catch(err => {
-    res.json(err);
-  });
+    db.Workout.create({})
+        .then(dbWorkout => {
+            //console.log(dbWorkout);
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
 });
 
 // app.put("/api/workouts/:id", ({body, params}, res) => {
@@ -73,18 +73,18 @@ app.put("/api/workouts/:id", (req, res) => {
     });
 });
 
-app.get("/api/workouts/range", ({query}, res) => {
-db.Workout.find({})
-.then(dbworkouts => {
-    res.json(dbworkouts);
-})
-.catch(err => {
-    res.json(err);
-});
+app.get("/api/workouts/range", ({ query }, res) => {
+    db.Workout.find({})
+        .then(dbworkouts => {
+            res.json(dbworkouts);
+        })
+        .catch(err => {
+            res.json(err);
+        });
 });
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
-  });
+});
 
 
